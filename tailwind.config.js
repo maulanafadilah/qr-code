08b08d3cba40142597bb1fd57d846ce036429c09
maulanafadilah <maulanafadilah@student.telkomuniversity.ac.id>
2022-11-02 10:48:41 +0700

    initialize

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#1f3251',
+        'secondary': '#7b879d',
+        'tertiary': '#d6e2f0',
+      },
+    },
+  },
+  plugins: [],
+}
